cc549d2931dd81783e212bd07d0635b39c080257
andykamp <akampenes@gmail.com>
2023-05-01 19:06:14 +0200

    chore: redeploy

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,6 +1,6 @@
 import "./global.css"
 
-export { default as Navbar } from "./Navbar"
-export { default as GoogleLoginButton } from "./GoogleLoginButton"
-export { default as Logo } from "./Logo"
-export { default as Profile } from "./Profile"
+export { default as Navbar } from "@/ui/Navbar"
+export { default as GoogleLoginButton } from "@/ui/GoogleLoginButton"
+export { default as Logo } from "@/ui/Logo"
+export { default as Profile } from "@/ui/Profile"
