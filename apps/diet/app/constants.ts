cc032c0924fc31d2606bc1bf9274f363ac45ebf3
andykamp <akampenes@gmail.com>
2023-05-01 18:38:12 +0200

    chore: force redeploy

--- apps/diet/app/constants.ts
+++ apps/diet/app/constants.ts
@@ -2,6 +2,7 @@ import type { Navigation } from '@/ui/Navbar';
 
 export const NAVIGATION_ROUTES_HOME: Navigation = [
 ];
+
 export const NAVIGATION_ROUTES_CONSOLE: Navigation = [
   { name: 'home', href: '/' },
   { name: 'console', href: '/console' },
