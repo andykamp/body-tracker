1cc4c06ea798d60c14f42f594c0dbcd382b02826
andykamp <akampenes@gmail.com>
2023-07-17 16:00:49 +0200

    feat: add batch set/upate/delete fireabe

--- libs/common/src/crud/crud.firebase.ts
+++ libs/common/src/crud/crud.firebase.ts
@@ -1,4 +1,4 @@
-import { doc, collection, getDoc, getDocs, setDoc, updateDoc, deleteDoc, type Firestore } from "firebase/firestore";
+import { doc, collection, getDoc, getDocs, setDoc, updateDoc, deleteDoc, writeBatch, type WriteBatch, type DocumentData, type DocumentReference, type Firestore } from "firebase/firestore";
 
 async function initializeDB(db: Firestore) {
   if (!crudApi.db) {
@@ -10,7 +10,6 @@ async function initializeDB(db: Firestore) {
 function getDocumentRef(collectionName: string, id: string) {
   return doc(crudApi.db, collectionName, id);
 }
-
 
 async function readCollection<T>(collectionName: string): Promise<T[]> {
   const collectionRef = collection(crudApi.db, collectionName);
@@ -49,6 +48,60 @@ async function deleteDocument(collectionName: string, id: string): Promise<void>
   return await deleteDoc(docRef);
 }
 
+// batch operations
+
+const BATCH_LIMIT = 500;
+type BatchOperationCallback = (
+  docRef: DocumentReference<DocumentData>,
+  batch: WriteBatch
+) => void;
+
+async function executeBatchedOperations(
+  collectionName: string,
+  data: { [id: string]: DocumentData },
+  operationCallback: BatchOperationCallback
+) {
+  let batch = writeBatch(crudApi.db);
+  let operationCount = 0;
+
+  for (const id in data) {
+    const docRef = doc(crudApi.db, collectionName, id)
+    operationCallback(docRef, batch);
+    operationCount++;
+
+    if (operationCount === BATCH_LIMIT) {
+      await batch.commit();
+      batch = writeBatch(crudApi.db);
+      operationCount = 0;
+    }
+  }
+
+  if (operationCount > 0) {
+    await batch.commit();
+  }
+}
+
+// Set
+async function setBatch(collection: string, data: { [id: string]: DocumentData }) {
+  await executeBatchedOperations(collection, data, (docRef, batch) => {
+    batch.set(docRef, data[docRef.id]);
+  });
+}
+
+// Update
+async function updateBatch(collection: string, data: { [id: string]: any }) {
+  await executeBatchedOperations(collection, data, (docRef, batch) => {
+    batch.update(docRef, data[docRef.id]);
+  });
+}
+
+// Delete
+async function deleteBatch(collection: string, data: { [id: string]: any }) {
+  await executeBatchedOperations(collection, data, (docRef, batch) => {
+    batch.delete(docRef);
+  });
+}
+
 const crudApi = {
   db: null as unknown as Firestore,
   initializeDB,
@@ -57,6 +110,11 @@ const crudApi = {
   readCollection,
   updateDocument,
   deleteDocument,
+
+  executeBatchedOperations,
+  setBatch,
+  updateBatch,
+  deleteBatch,
 }
 
 export type CrudApi = typeof crudApi;
