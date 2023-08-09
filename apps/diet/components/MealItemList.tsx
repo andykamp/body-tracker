1060817632674a8ca06b4a8de4634e05e852d0b7
andykamp <akampenes@gmail.com>
2023-08-09 20:45:49 +0200

    refactor: wip mutating cache

--- apps/diet/components/MealItemList.tsx
+++ apps/diet/components/MealItemList.tsx
@@ -23,16 +23,59 @@ function MealItemList(props: MealItemListProps) {
   // @todo: do i need to use cache here?
   const addProductMutation = useMutation({
     mutationFn: productApi.addProduct,
-    onSuccess: () => {
-      // Invalidate and refetch
+    onMutate: async (mutated) => {
+      console.log('onMutate', mutated)
+      // to update
+      // // Cancel any outgoing refetches
+      // (so they don't overwrite our optimistic update)
+      // await queryClient.cancelQueries({ queryKey: ['todos'] })
+
+      // // Snapshot the previous value
+      // const previousProducts = queryClient.getQueryData(cacheKey) as t.Product[]
+
+      // const updatedProducts = [...previousProducts]
+      // const index = updatedProducts.findIndex((p) => p.id === mutated.product.id)
+
+      // if (index !== -1) {
+      //   updatedProducts[index] = {
+      //     ...updatedProducts[index],
+      //     ...mutated.product,
+      //   }
+      // // Optimistically update to the new value
+      //   queryClient.setQueryData(cacheKey, updatedProducts)
+      // }
+      // return () => queryClient.setQueryData(cacheKey, previousProducts)
+
+      const cacheKey = ["getProductForCurrentUser"]
+      // to add
+      // Cancel any outgoing refetches
+      // (so they don't overwrite our optimistic update)
+      await queryClient.cancelQueries({ queryKey: cacheKey })
+
+      // Snapshot the previous value
+      const previousProducts = queryClient.getQueryData(cacheKey) as t.Product[]
+
+      // Optimistically update to the new value
+      const updatedProducts = [...previousProducts, mutated.product]
+      queryClient.setQueryData(cacheKey, updatedProducts)
+
+      return () => queryClient.setQueryData(cacheKey, previousProducts)
+
+    },
+    onSettled: (_data, error) => {
+      // Always refetch after error or success:
+      if(error){
+        alert('addPrdouctMutation error')
+      }
+      console.log('onSuccess')
       queryClient.invalidateQueries({ queryKey: ['getProductForCurrentUser'] })
       queryClient.invalidateQueries({ queryKey: ['getDaily'] })
     },
   })
 
   const updateMeal = (meal: t.Meal) => {
-    const macros =  mealApi.getMacros(meal)
-    const newMeal = {...meal, ...macros}
+    const macros = mealApi.getMacros(meal)
+    const newMeal = { ...meal, ...macros }
     onMealChange(newMeal)
   }
 
@@ -40,15 +83,16 @@ function MealItemList(props: MealItemListProps) {
     // create a ewn product
     const newProduct = createEmptyProduct(true)
 
+    console.log('___________')
     // store product
-    // addProductMutation.mutate({
-    //   userId: user.uid,
-    //   product: newProduct
-    // })
-    await productApi.addProduct({
+    addProductMutation.mutate({
       userId: user.uid,
       product: newProduct
     })
+    // await productApi.addProduct({
+    //   userId: user.uid,
+    //   product: newProduct
+    // })
 
     // crate a item around it
     const newItem = itemApi.createItemWrapper(newProduct, "product")
