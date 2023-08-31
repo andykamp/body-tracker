0e1024f8c1155a631c6e6fc0f69b6f0dc93158e7
andykamp <akampenes@gmail.com>
2023-08-31 20:41:32 +0200

    feat: disable next button on current day

--- apps/diet/components/DailyPicker.tsx
+++ apps/diet/components/DailyPicker.tsx
@@ -1,3 +1,4 @@
+import dailyApi from "@/diet-server/daily/daily.api";
 import React from "react";
 
 type DailyPickerProps = {
@@ -5,17 +6,18 @@ type DailyPickerProps = {
   onPrev: () => void
   onNext: () => void
 }
-
+const todayKey = dailyApi.getTodaysDailyKey()
 function DailyPicker (props: DailyPickerProps) {
   const { dailyKey, onNext, onPrev} = props
 
   const readableData = dailyKey
 
+  const isNextDisabled = dailyKey === todayKey
   return (
     <div>
       <button onClick={onPrev}>prev</button>
       {readableData}
-      <button onClick={onNext}>next</button>
+      <button className={isNextDisabled?'text-gray-400 cursor-not-allowed':''}disabled={isNextDisabled} onClick={onNext}>next</button>
     </div >
   )
 }
