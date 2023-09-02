dd757f01ca49f40cfa3c39d00c713e09a9e643e6
andykamp <akampenes@gmail.com>
2023-09-02 18:20:36 +0200

    feat: disable prev 7 day max

--- apps/diet/components/DailyPicker.tsx
+++ apps/diet/components/DailyPicker.tsx
@@ -1,5 +1,6 @@
 import dailyApi from "@/diet-server/daily/daily.api";
 import React from "react";
+import { adjustDateByXDays } from "@/diet-server/daily/daily.utils";
 
 type DailyPickerProps = {
   dailyKey: string
@@ -7,17 +8,30 @@ type DailyPickerProps = {
   onNext: () => void
 }
 const todayKey = dailyApi.getTodaysDailyKey()
-function DailyPicker (props: DailyPickerProps) {
-  const { dailyKey, onNext, onPrev} = props
+function DailyPicker(props: DailyPickerProps) {
+  const { dailyKey, onNext, onPrev } = props
 
   const readableData = dailyKey
 
   const isNextDisabled = dailyKey === todayKey
+  const isSevenDaysAgo = dailyKey === adjustDateByXDays(todayKey, -7)
   return (
     <div>
-      <button onClick={onPrev}>prev</button>
+      <button
+        className={isSevenDaysAgo ? 'text-gray-400 cursor-not-allowed' : ''}
+        disabled={isSevenDaysAgo}
+        onClick={onPrev}
+      >
+        prev
+      </button>
       {readableData}
-      <button className={isNextDisabled?'text-gray-400 cursor-not-allowed':''}disabled={isNextDisabled} onClick={onNext}>next</button>
+      <button
+        className={isNextDisabled ? 'text-gray-400 cursor-not-allowed' : ''}
+        disabled={isNextDisabled}
+        onClick={onNext}
+      >
+        next
+      </button>
     </div >
   )
 }
