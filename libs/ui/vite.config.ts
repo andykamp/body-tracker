7889ace27f2c0a0ab7e2ef787f772d16190d1374
andykamp <akampenes@gmail.com>
2023-09-16 17:43:03 +0200

    refactor: upate ui:build command

--- libs/ui/vite.config.ts
+++ libs/ui/vite.config.ts
@@ -34,9 +34,10 @@ export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points.
-      entry: 'src/index.ts',
+      entry: "src/*",
+      // Could also be a dictionary or array of multiple entry points.
       name: 'ui',
-      fileName: 'index',
+      // fileName: 'index',
       // Change this to the formats you want to support.
       // Don't forgot to update your package.json as well.
       formats: ['es', 'cjs'],
